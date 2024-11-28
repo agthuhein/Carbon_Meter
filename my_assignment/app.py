9ae47bd216d9ebd0b94084d450f670a652f8af3f
agthuhein <agthuhein.mm@gmail.com>
2024-11-28 12:16:11 +0100

    dashboard last 3 moths, and last 6 months select box action

--- my_assignment/app.py
+++ my_assignment/app.py
@@ -3,6 +3,7 @@ from model import db, app, User, Company, Energy, Waste, BusinessTravel, Usage
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 import random
+import pandas as pd
 
 
 ###Views###
@@ -340,95 +341,140 @@ def get_data():
                 total.append(t)
                 color.append(get_random_rgb())
 
-            pie_data = {"labels": company_name, "values": total, "color": color, "label": "kgCO2FD"}
+            pie_data = {"labels": company_name, "values": total, "color": color, "label": "kgCO2"}
+            #print(jsonify(pie_data))
             return jsonify(pie_data)
-
+#for page load table data
 @app.route('/get_table_data', methods=['GET'])
 def get_table_data():
     thisMonth = datetime.now()
-    thisMonthNum = thisMonth.strftime("%m")
     lastMonth = thisMonth - relativedelta(months=1)
     lastMonthNum = lastMonth.strftime("%m")
 
     with app.app_context():
         t_record = db.session.query(Company, Usage).join(Usage, Usage.company_id == Company.id).filter(Usage.month == lastMonthNum).all()
-        comp_name = []
-        comp_sect = []
-        energy = []
-        waste = []
-        fuel = []
         tableData = []
+
     
         for c, u in t_record:
-            comp_name.append(c.name)
-            comp_sect.append(c.sector)
-            energy.append(u.energy)
-            waste.append(u.waste)
-            fuel.append(u.fuel)
-            eachCompany = {"name": c.name, "sector": c.sector, "energy": u.energy, "waste": u.waste, "fuel": u.fuel}
+            eachCompany = {"name": c.name, "sector": c.sector, "energy": u.energy, "waste": u.waste, "fuel": u.fuel, "month": u.month, "year": u.year}
             tableData.append(eachCompany)
-        
-        t_data = {"comp_name": comp_name, "sector": comp_sect, "energy": energy, "waste": waste, "fuel": fuel}
 
-        print(t_data)
     return jsonify(tableData)
 
-def get_random_rgb():
-    return "#{:06x}".format(random.randint(0, 0xFFFFFF))
-##Get Dashboard Data
-def get_dashboard_data(time_range):
-
-    # Get the current date and time
-    thisMonth = datetime.now()
-
-    # Get the current month number (as a string)
-    thisMonthNum = thisMonth.strftime("%m")
-    print(f"Current Month: {thisMonthNum}")
-
-    # Calculate the previous month using relativedelta
-    lastMonth = thisMonth - relativedelta(months=1)
-    lastMonthNum = lastMonth.strftime("%m")
-    print(f"Last Month: {lastMonthNum}")
-    current_date = datetime.now()
-
-    if time_range == "last_3_month":
+#last 3 months, all companies, for pie and bar
+@app.route('/calculate_l3_ac', methods=['GET'])
+def calculate_l3_ac():
+    company_totals = {}
+    three_months = get_last_three_months_with_year()
+    color = []
+    for m in three_months:
+        year, month = m
+        
         with app.app_context():
-            user = User.query.filter_by(email = session['email']).first()
-            companies = Company.query.all()
+            records = db.session.query(Company.name, Company.sector, Usage.energy, Usage.waste, Usage.fuel).join(Usage, Usage.company_id == Company.id).filter(Usage.month == month and Usage.year == year).all()
+            # print(records)
+            for record in records:
+                
+                company, sector, *values = record
+                total_for_entry = sum(values)
+                if company not in company_totals:
+                    company_totals[company] = {"sector": sector, "total": 0, "color": get_random_rgb()}
+                company_totals[company]["total"] += total_for_entry
+             
+            result = [{"labels": company, "sector": info["sector"], "color": info["color"],"values": round(info["total"],2), "label": "kgCO2"} for company, info in company_totals.items()] 
+            #result_json = json.dumps(result, indent=4)
+            #pie_data = {"labels": company, "values": round(total_for_entry,2), "color": color, "label": "kgCO2"}
 
-            record = db.session.query(Company, Usage).join(Usage, Usage.company_id == Company.id).filter(Usage.month == lastMonthNum).all()
-            total = []
-            company_name = []
-            color = []
+    #print(result)
+    return jsonify(result)
 
-            for c, u in record:
-                company_name.append(c.name)
-                t = round((u.energy + u.waste + u.fuel),2)
-                total.append(t)
-                color.append(get_random_rgb())
+#for last 3 months table data
+@app.route('/get_table_data_l3m', methods=['GET'])
+def get_table_data_l3m():
+    tableData = []
+    t_records=[]
+    three_months = get_last_three_months_with_year()
+    for m in three_months:
+        year, month = m
+        
+        with app.app_context():
+            t_records = db.session.query(Company.name, Company.sector, Usage.energy, Usage.waste, Usage.fuel, Usage.month, Usage.year).join(Usage, Usage.company_id == Company.id).filter(Usage.month == month and Usage.year == year).all()
+            
+            for r in t_records:
+                eachCompany = {"name": r.name, "sector": r.sector, "energy": r.energy, "waste": r.waste, "fuel": r.fuel, "month": r.month, "year": r.year}
+            
+                tableData.append(eachCompany)
+    return jsonify(tableData)
 
-            pie_data = {"labels": company_name, "values": total, "color": color, "label": "kgCO2TT"}
-        return pie_data
-    elif time_range == "last_month":
-        return {"title": "Last 3 Month"}
-    elif time_range == "last_6_month":
-        return {"title": "Last 3 Month"}
-    else:
-        return {"title": "Unknown Range"}
 
-@app.route('/update_dashboard', methods=["GET","POST"])
-def update_dashboard():
-    time_range = request.json.get("selectedMonth")
-    jdata = get_dashboard_data(time_range)
-    return jdata
+#last 6 months, all companies, for pie and bar
+@app.route('/calculate_l6_ac', methods=['GET'])
+def calculate_l6_ac():
+    company_totals = {}
+    six_months = get_last_six_months_with_year()
+    color = []
+    for m in six_months:
+        year, month = m
+        
+        with app.app_context():
+            records = db.session.query(Company.name, Company.sector, Usage.energy, Usage.waste, Usage.fuel).join(Usage, Usage.company_id == Company.id).filter(Usage.month == month and Usage.year == year).all()
+            # print(records)
+            for record in records:
+                
+                company, sector, *values = record
+                total_for_entry = sum(values)
+                if company not in company_totals:
+                    company_totals[company] = {"sector": sector, "total": 0, "color": get_random_rgb()}
+                company_totals[company]["total"] += total_for_entry
+             
+            result = [{"labels": company, "sector": info["sector"], "color": info["color"],"values": round(info["total"],2), "label": "kgCO2"} for company, info in company_totals.items()] 
+    return jsonify(result)
 
+
+#for last 6 months table data
+@app.route('/get_table_data_l6m', methods=['GET'])
+def get_table_data_l6m():
+    tableData = []
+    t_records=[]
+    three_months = get_last_six_months_with_year()
+    for m in three_months:
+        year, month = m
+        
+        with app.app_context():
+            t_records = db.session.query(Company.name, Company.sector, Usage.energy, Usage.waste, Usage.fuel, Usage.month, Usage.year).join(Usage, Usage.company_id == Company.id).filter(Usage.month == month and Usage.year == year).all()
+            
+            for r in t_records:
+                eachCompany = {"name": r.name, "sector": r.sector, "energy": r.energy, "waste": r.waste, "fuel": r.fuel, "month": r.month, "year": r.year}
+            
+                tableData.append(eachCompany)
+    return jsonify(tableData)
+
+
+#get last three months. months and years
 def get_last_three_months_with_year():
     today = datetime.today()
     last_three_months = [
         ((today - relativedelta(months=i)).year, (today - relativedelta(months=i)).month)
         for i in range(1, 4)
     ]
+    # print(last_three_months)
     return last_three_months
+
+
+#get last six months. months and years
+def get_last_six_months_with_year():
+    today = datetime.today()
+    last_six_months = [
+        ((today - relativedelta(months=i)).year, (today - relativedelta(months=i)).month)
+        for i in range(1, 7)  # Change this to 7 to include the last 6 months
+    ]
+    return last_six_months
+
+
+#get random color
+def get_random_rgb():
+    return "#{:06x}".format(random.randint(0, 0xFFFFFF))
 
 
 if __name__ == "__main__":
